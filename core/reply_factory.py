163e1a0577a3eb3475f326be6636ca1871146637
kishoremalimelu <150910130+kishoremalimelu@users.noreply.github.com>
2024-07-10 21:58:59 +0530

    Update reply_factory.py

--- core/reply_factory.py
+++ core/reply_factory.py
@@ -32,6 +32,15 @@ def record_current_answer(answer, current_question_id, session):
     '''
     Validates and stores the answer for the current question to django session.
     '''
+    if not isinstance(answer,str):
+        return False," answer must be a string"
+    if current_question_id is None:
+        return False,"current question ID is not set"
+    if current_question_id>= len(PYTHON_QUESTION_LIST) or current_question_id<0:
+        return False,"Ivalid question ID."
+    if "answers" not in session:
+        session["answer"]={}
+    session["answer"][current_question_id]=answer
     return True, ""
 
 
@@ -39,8 +48,13 @@ def get_next_question(current_question_id):
     '''
     Fetches the next question from the PYTHON_QUESTION_LIST based on the current_question_id.
     '''
+    next_question_id= current_question_id +1
+    if next_quedtion_id>=len(PYTHON_QUESTION_LIST):
+        return None,None
+    next_question=PYTHON_QUEZTION_LIST[next_question_id]
+    
 
-    return "dummy question", -1
+    return next_question,next_question_id
 
 
 def generate_final_response(session):
@@ -48,5 +62,6 @@ def generate_final_response(session):
     Creates a final result message including a score based on the answers
     by the user for questions in the PYTHON_QUESTION_LIST.
     '''
-
-    return "dummy result"
+    answers = session.get("answer",{})
+    score=sum(1 for q_id, ans in answers.items() if ans)
+    return f"Final result:{score} out of {len(PYTHON_QUESTION_LIST)}"
